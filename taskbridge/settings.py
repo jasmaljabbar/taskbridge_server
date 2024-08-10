9a7084aa11ad2a735494e785aa5abaaa920cecfe
jasmaljabbar <jasmaljabbar24@gmail.com>
2024-08-10 10:56:02 +0530

    make change in settings

--- taskbridge/settings.py
+++ taskbridge/settings.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -91,6 +92,16 @@ CHANNEL_LAYERS = {
         "CONFIG": {
             "hosts": [(REDIS_HOST, REDIS_PORT)],
         },
+    },
+}
+
+
+STORAGES = {
+    "default":{
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND":"whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
