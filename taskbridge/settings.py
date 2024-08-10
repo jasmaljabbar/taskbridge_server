5d834b289b2ef724a1dadf5c303950dba3b5eac6
jasmaljabbar <jasmaljabbar24@gmail.com>
2024-08-10 09:45:01 +0530

    databse details changed

--- taskbridge/settings.py
+++ taskbridge/settings.py
@@ -124,7 +124,7 @@ DATABASES = {
         'USER': env("DATABASE_USER"),
         'PASSWORD': env("DATABASE_PASSWORD"),
         'HOST': env("DATABASE_HOST"), 
-        'PORT': '5432',
+        'PORT': env("DATABASE_PORT"),
     }
 }
 
