be126b78b8cbb7b148cfcd9242d77fe62e3e82cf
jasmaljabbar <jasmaljabbar24@gmail.com>
2024-08-10 09:00:10 +0530

    requirements.txt changed

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taskbridge.settings")
+    settings_module = 'taskbridge.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'taskbridge.settings'
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- taskbridge/wsgi.py
+++ taskbridge/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taskbridge.settings")
+settings_module = 'taskbridge.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'taskbridge.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_wsgi_application()
