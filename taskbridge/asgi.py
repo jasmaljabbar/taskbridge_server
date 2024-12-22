b00bbad42549891aca8600e674789d71899340c0
jasmal jabbar <jasmaljabbar24@gmil.com>
2024-12-22 09:56:25 +0530

    change agi and routing

--- taskbridge/asgi.py
+++ taskbridge/asgi.py
@@ -1,20 +1,20 @@
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taskbridge.settings")
+
+# Import and setup django before any other imports
+import django
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import re_path
-from chat.consumers import TextConsumer
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taskbridge.settings")
-
-import django
-django.setup()
+from chat.consumers import TextConsumer, NotificationConsumer
 
 websocket_urlpatterns = [
     re_path(r'^ws/chat/(?P<room_name>[\w_]+)/$', TextConsumer.as_asgi()),
+    re_path(r'^ws/notifications/(?P<room_name>[\w_]+)/$', NotificationConsumer.as_asgi()),
 ]
-
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
