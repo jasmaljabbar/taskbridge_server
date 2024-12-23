84d66fe72570622a12840528692afbba01e2dcdb
jasmal jabbar <jasmaljabbar24@gmil.com>
2024-12-23 09:38:14 +0530

    change default file back for test

--- taskbridge/settings.py
+++ taskbridge/settings.py
@@ -261,7 +261,8 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': env('CLOUDINARY_API_SECRET')
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+
 
 
 LOGGING = {
