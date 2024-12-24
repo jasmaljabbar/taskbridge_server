e7c02f2c986d090174003f12e78e69d26eae75e8
jasmal jabbar <jasmaljabbar24@gmil.com>
2024-12-24 11:56:28 +0530

    add _ of gettext_lazy in userdata model

--- account/models.py
+++ account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.core.validators import RegexValidator
 
